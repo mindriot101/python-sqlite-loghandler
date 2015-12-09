a974c40cbc6f99e329785665536dc61170ff1210
Simon Walker <s.r.walker101@googlemail.com>
2015-12-09 15:06:57 +0000

    Add extra packaging metadata

--- setup.py
+++ setup.py
@@ -4,5 +4,14 @@ setup(
     name='sqlitehandler',
     author='Simon Walker',
     author_email='s.r.walker101@googlemail.com',
+    version='0.0.1',
+    license='MIT',
     py_modules=['sqlitehandler'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
 )
